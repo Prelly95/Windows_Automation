a33e4582c7a7e3b6e8db28725f8b08c0cc1da7ba
PatrickPrell <patrickprell@corridor.ai>
2020-08-21 10:38:27 +1000

    adds git_shell in wt to context menu anf adds wt to folder context menu

--- Windows_Terminal_Context/add_wt_context.ps1
+++ Windows_Terminal_Context/add_wt_context.ps1
@@ -13,3 +13,52 @@ if (-not (Test-Path -Path "Registry::HKEY_CLASSES_ROOT\Directory\Background\shel
 	New-Item -Path "Registry::HKEY_CLASSES_ROOT\Directory\Background\shell\wt\command" -Force
 }
 Set-Item -Path "Registry::HKEY_CLASSES_ROOT\Directory\Background\shell\wt\command" -Value $command -Force
+
+$command = "$env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\Microsoft.WindowsTerminal_8wekyb3d8bbwe\wt.exe -p `"Git Bash`" -d .\"
+$icon_path = "$env:PROGRAMFILES\Git\git-bash.exe"
+
+if (-not (Test-Path -Path "Registry::HKEY_CLASSES_ROOT\Directory\Background\shell\git_shell"))
+{
+	New-Item -Path "Registry::HKEY_CLASSES_ROOT\Directory\Background\shell\git_shell" -Force
+}
+Set-Item -Path "Registry::HKEY_CLASSES_ROOT\Directory\Background\shell\git_shell" -Value "Git Ba&sh Here" -Force
+New-ItemProperty -Path "Registry::HKEY_CLASSES_ROOT\Directory\Background\shell\git_shell" -Name icon -PropertyType String -Value $icon_path -Force
+
+if (-not (Test-Path -Path "Registry::HKEY_CLASSES_ROOT\Directory\Background\shell\git_shell\command"))
+{
+	New-Item -Path "Registry::HKEY_CLASSES_ROOT\Directory\Background\shell\git_shell\command" -Force
+}
+Set-Item -Path "Registry::HKEY_CLASSES_ROOT\Directory\Background\shell\git_shell\command" -Value $command -Force
+
+
+$command = "$env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\Microsoft.WindowsTerminal_8wekyb3d8bbwe\wt.exe -p `"Command Prompt`" -d %V"
+$icon_path = "$env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\Microsoft.WindowsTerminal_8wekyb3d8bbwe\Windows-Terminal-icon.ico"
+
+if (-not (Test-Path -Path "Registry::HKEY_CLASSES_ROOT\Directory\shell\wt"))
+{
+	New-Item -Path "Registry::HKEY_CLASSES_ROOT\Directory\shell\wt" -Force
+}
+Set-Item -Path "Registry::HKEY_CLASSES_ROOT\Directory\shell\wt" -Value "Open Windows Terminal Here" -Force
+New-ItemProperty -Path "Registry::HKEY_CLASSES_ROOT\Directory\shell\wt" -Name icon -PropertyType String -Value $icon_path -Force
+
+if (-not (Test-Path -Path "Registry::HKEY_CLASSES_ROOT\Directory\shell\wt\command"))
+{
+	New-Item -Path "Registry::HKEY_CLASSES_ROOT\Directory\shell\wt\command" -Force
+}
+Set-Item -Path "Registry::HKEY_CLASSES_ROOT\Directory\shell\wt\command" -Value $command -Force
+
+$command = "$env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\Microsoft.WindowsTerminal_8wekyb3d8bbwe\wt.exe -p `"Git Bash`" -d %V"
+$icon_path = "$env:PROGRAMFILES\Git\git-bash.exe"
+
+if (-not (Test-Path -Path "Registry::HKEY_CLASSES_ROOT\Directory\shell\git_shell"))
+{
+	New-Item -Path "Registry::HKEY_CLASSES_ROOT\Directory\shell\git_shell" -Force
+}
+Set-Item -Path "Registry::HKEY_CLASSES_ROOT\Directory\shell\git_shell" -Value "Git Ba&sh Here" -Force
+New-ItemProperty -Path "Registry::HKEY_CLASSES_ROOT\Directory\shell\git_shell" -Name icon -PropertyType String -Value $icon_path -Force
+
+if (-not (Test-Path -Path "Registry::HKEY_CLASSES_ROOT\Directory\shell\git_shell\command"))
+{
+	New-Item -Path "Registry::HKEY_CLASSES_ROOT\Directory\shell\git_shell\command" -Force
+}
+Set-Item -Path "Registry::HKEY_CLASSES_ROOT\Directory\shell\git_shell\command" -Value $command -Force
---
+++ disable_capslock/disable_capslock.ps1
@@ -0,0 +1,9 @@
+$path = "HKLM:\SYSTEM\CurrentControlSet\Control\Keyboard Layout"
+
+if (-not (Test-Path -Path "$path"))
+{
+	New-Item -Path $path -Force
+}
+New-ItemProperty -Path $path -Name "Scancode Map" -Type Binary -Value ([byte[]](00, 00, 00, 00, 00, 00, 00, 00, 02, 00, 00, 00, 00, 00, 58, 00, 00, 00, 00, 00)) -Force
+
+pause
